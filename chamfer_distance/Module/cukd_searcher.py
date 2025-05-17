64b02f4ad3778bcde3fa849bd5cb50127b85ba06
chLi <565353780@qq.com>
2025-05-17 20:49:48 +0800

    fix init bug

--- chamfer_distance/Module/cukd_searcher.py
+++ chamfer_distance/Module/cukd_searcher.py
@@ -7,5 +7,6 @@ from chamfer_distance.Module.base_searcher import BaseSearcher
 
 class CUKDSearcher(BaseSearcher):
     def __init__(self):
+        super().__init__()
         self.searcher = chamfer_cpp.CUKDSearcher()
         return
