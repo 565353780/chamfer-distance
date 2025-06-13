312d05d4612eeade1013a16084a5136c1d66b182
chLi <565353780@qq.com>
2025-06-13 15:39:58 +0800

    add large batch test, cukd searcher is the best one

--- chamfer_distance/Demo/cukd_searcher.py
+++ chamfer_distance/Demo/cukd_searcher.py
@@ -6,9 +6,28 @@ from chamfer_distance.Module.cukd_searcher import CUKDSearcher
 from chamfer_distance.Module.chamfer_distances import ChamferDistances
 
 
+def testAlgoSpeed(
+    xyz1: torch.Tensor,
+    xyz2: torch.Tensor,
+    algo_name: str,
+    iter_num: int = 10,
+) -> bool:
+    print("start calculate chamfer via " + algo_name + "...")
+    start_time = time.time()
+    for _ in trange(iter_num):
+        dists1, dists2, idxs1, idxs2 = ChamferDistances.namedAlgo(algo_name)(xyz1, xyz2)
+        mean = dists1.mean() + dists2.mean()
+        assert mean >= 0
+    spend_time = time.time() - start_time
+    spend_time /= iter_num
+
+    print("time:", spend_time, "s; fps:", 1.0 / spend_time)
+    return True
+
+
 def demo(
     baseline_name: str = "cukd",
-    xyz_shapes: list = [300000, 400000],
+    xyz_shapes: list = [1600, 1000],
     iter_num: int = 10,
     device: str = "cuda:0",
 ):
@@ -19,43 +38,29 @@ def demo(
 
     # 创建两个点云
     n_points, m_points = xyz_shapes
-    xyz1 = torch.rand(10, n_points, 3, device=device).float()  # 查询点云
-    xyz2 = torch.rand(10, m_points, 3, device=device).float()  # 静态点云
+    xyz1 = torch.rand(10000, n_points, 3, device=device).float()  # 查询点云
+    xyz2 = torch.rand(10000, m_points, 3, device=device).float()  # 静态点云
 
     xyz1.requires_grad_(True)
 
     print(f"点云1形状: {xyz1.shape}")
     print(f"点云2形状: {xyz2.shape}")
 
-    # 使用传统方法计算Chamfer距离
-    print("\n使用传统方法计算Chamfer距离...")
-    start_time = time.time()
-    for _ in trange(iter_num):
-        dists1, dists2, idxs1, idxs2 = ChamferDistances.namedAlgo(baseline_name)(
-            xyz1, xyz2
-        )
-        mean = dists1.mean() + dists2.mean()
-        assert mean >= 0
-    traditional_time = time.time() - start_time
-    traditional_time /= iter_num
-    print(f"传统方法耗时: {traditional_time:.4f}秒")
+    # testAlgoSpeed(xyz1, xyz2, "default", iter_num)
+    testAlgoSpeed(xyz1, xyz2, "cuda", iter_num)
+    # testAlgoSpeed(xyz1, xyz2, "triton", iter_num)
+    testAlgoSpeed(xyz1, xyz2, "cukd", iter_num)
 
-    # 使用新的CUKDSearcher类
     print("\n使用新的Searcher类...")
-    start_time = time.time()
-
     searcher = CUKDSearcher()
+    searcher.addPoints(xyz2)
 
-    # 添加静态点云
-    searcher.addPoints(xyz2)  # 将批次维度展平
-
-    # 查询最近邻点
+    start_time = time.time()
     for _ in trange(iter_num):
-        dist1, dist2, idx1, idx2 = searcher.query(xyz1)  # 将批次维度展平
+        dist1, dist2, idx1, idx2 = searcher.query(xyz1)
         mean = dist1.mean() + dist2.mean()
         assert mean >= 0
     new_method_time = time.time() - start_time
     new_method_time /= iter_num
-    print(f"新方法耗时: {new_method_time:.4f}秒")
-    print("speed up ratio:", traditional_time / new_method_time)
+    print("time:", new_method_time, "s; fps:", 1.0 / new_method_time)
     return True
