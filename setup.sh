4dfd7ac25227632b8e0ab40ea13460b14f351619
chLi <565353780@qq.com>
2025-06-17 21:36:40 +0800

    update the torch version

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 \
+pip install torch torchvision torchaudio \
   --index-url https://download.pytorch.org/whl/cu124
 
 pip install -U icecream jax
